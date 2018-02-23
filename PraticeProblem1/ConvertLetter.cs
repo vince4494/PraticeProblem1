131edb4bf6afd41d683e2f4e4e746436d9e780b2
vince.zipparro <vince.zipparro@GXPC024380>
2018-02-23 16:12:28 -0600

    Add project files.

---
+++ PraticeProblem1/ConvertLetter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace PraticeProblem1
+{
+    public class ConvertLetter
+    {
+
+        public string upperCaseLetter(string letter)
+        {
+            string exception = "Input is not a letter";
+
+            if(Regex.IsMatch(letter, @"^[a-zA-Z]+$"))
+            {
+                return letter.ToUpper();
+            }
+            else
+            {
+                return exception;
+            }
+            
+        }
+
+    }
+}
---
+++ PraticeProblem1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticeProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            Console.WriteLine("Type in a Letter:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Capitalized Letter:");
+            ConvertLetter convertLetterFromUserInput = new ConvertLetter();
+            output = convertLetterFromUserInput.upperCaseLetter(input);
+            Console.WriteLine(output);
+
+
+
+        }
+    }
+}
